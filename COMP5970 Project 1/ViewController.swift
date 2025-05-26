ac033b8eb1c065a845245d0700c30337320a5432
Garrett Li <166572446+Li-Guang-Han@users.noreply.github.com>
2025-05-25 23:13:26 -0500

    Uplaod from Desktop
    Project 1

---
+++ COMP5970 Project 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COMP5970 Project 1
+//
+//  Created by Garrett Li on 5/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
